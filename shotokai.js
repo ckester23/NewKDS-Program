88b441d4789531fd1d6da1d05e4636c5cba39a70
Cheyanne Kester <ckester@uoregon.edu>
2022-04-25 12:46:34 -0700

    added all files

---
+++ shotokai.js
@@ -0,0 +1,59 @@
+// JavaScript file for shotokai project
+// string 'hello' character "h"
+// false = 0, "", NaN, null, and undefined when used in bools
+
+let stances = [ Seiza = "Seiza.mp3",
+	Hachiji_dachi = "Hachiji_dachi.mp3",
+	Kiba_dachi = "Kiba_dachi.mp3",
+	Zenkutsu_dachi = "Zenkutsu.mp3",
+	Kokutsu_dachi = "Kokutsu_dachi.mp3",
+	Fudo_dachi = "Fudo_dachi.mp3" ]
+
+let hand_techniques = [ Oi_tsuki = "Oi_tsuki.mp3",
+	Mae_te_tsuki = "Mae_te_tsuki.mp3",
+	Gyaku_tsuki = "Gyaku_tsuki.mp3",
+	Uraken = "Uraken",
+	Empi = "Empi",
+	Shomen_uchi = "Shomen Uchi",
+	Yokomen_uchi = "Yokomen Uchi",
+	Tetsui = "Tetsui",
+	Morote_tsuki = "Morote Tsuki",
+	Yama_tsuki = "Yama Tsuki" ]
+
+let kicks = [ Mae_geri = "Mae_geri.mp3",
+	Yoko_geri_kekomi = "Yoko_geri_kekomi.mp3",
+	Yoko_geri_keage = "Keage",
+	Fumikomi = "Fumi_komi.mp3",
+	Mawashi_geri = "Mawashi",
+	Mikazuki_geri = "Mikazuki",
+	Aori_geri = "Aori",
+	Nami_gaeshi = "Nami Gaeshi",
+	Hiza_tsuchi = "Hiza Tsuchi",
+	Nidan_geri = "Nidan" ]
+
+let blocks = [ Gedan_barai = "Gedan_barai.mp3",
+	Teisho_barai = "teisho_barai.mp3",
+	Udi_uke = "Udi_uke.mp3",
+	Uchikomi = "Uchikomi.mp3",
+	Age_uke = "Age_uke.mp3",
+	Shuto_uke = "Shuto Uke",
+	Shuto_barai = "Shuto Barai",
+	Morote_uke = "Morote Uke" ]
+
+let general = [ T_ShodanTate_tsuki = "Tate_tsuki",
+	Seiken = "Seiken",
+	Hiki_te = "Hiki_te.mp3",
+	Gedan = "Gedan",
+	Chudan = "Chudan",
+	Jodan = "Jodan",
+	Irimi = "Irimi.mp3" ]
+
+let Taikyoku = [ T_Shodan = "Taikyoku_Shodan.mp3",
+	T_Nidan = "Taikyoku_Nidan.mp3",
+	T_Sandan = "Taikyoku_Sandan.mp3" ]
+
+let Heian = [ H_Shodan = "Heian_Shodan.mp3",
+	H_Nidan = "Heian_Nidan.mp3",
+	H_Sandan = "Heian_Sandan.mp3" ]
+
+let Tekki = [ Te_Shodan = "Tekki_Shodan.mp3" ]
